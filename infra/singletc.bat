795ee664914950ffdb3b6d6eabf1a3af9586f223
sargunkour <sargunkoursasan98@gmail.com>
2025-04-03 02:56:09 +0530

    ReadME

--- infra/singletc.bat
+++ infra/singletc.bat
@@ -1,2 +1,2 @@
 cd C:\Robot-Incubyte-FW\TestCases
-C:\Python\python.exe -m robot -t "Verify User Should Be Able To SignIn Successfully" -d "C:\ZResults" test1.robot
+C:\Python\python.exe -m robot -t "Verify User Should Be Able To SignIn Successfully" -d "C:\ZResults\UI" test1.robot
